f7e3a9589dda980f0bd3db23a10557b27feef04f
Andreas Heizmann <andreasheizmann90@gmail.com>
2025-01-13 20:44:25 +0000

    feat: added request header parser

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.use("/api/whoami", function express(req,res,next) {
+  res.json({ipaddress: req.ip, language: req.headers["accept-language"], software: req.headers["user-agent"]});
+  next();
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
